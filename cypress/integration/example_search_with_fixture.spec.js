17d129ac1094f199ef79b921cbead0de1d357481
alvaro <almamani@agetic.gob.bo>
2020-09-06 22:59:53 -0400

    adicion de ejemplo de alias en fixture

--- cypress/integration/example_search_with_fixture.spec.js
+++ cypress/integration/example_search_with_fixture.spec.js
@@ -1,13 +1,22 @@
 describe('Prueba de busqueda', () => {
   beforeEach(() => {
     cy.visit('/');
+    cy.fixture('search.json').as('searchData');
   });
-  it('Deberia buscar un texto en el input de busqueda', () => {
+  it('Deberia buscar un texto primario del fixture search en el input de busqueda', () => {
     cy.fixture('search.json').then(data => {
       cy.log(data);
       cy.get('#search_query_top').type(data.primary);
       cy.get('#searchbox > .btn').click();
       cy.get('.lighter').contains(data.primary);
+    });
+  });
+  it('Deberia buscar un texto secundario del fixture search en el input de busqueda', () => {
+    cy.get('@searchData').then(data => {
+      cy.log(data);
+      cy.get('#search_query_top').type(data.secondary);
+      cy.get('#searchbox > .btn').click();
+      cy.get('.lighter').contains(data.secondary);
     });
   });
 });
